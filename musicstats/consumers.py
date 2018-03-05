a717b3ff0a6047b85df68d679e2f99a9539750d3
Marc Steele <m.steele@bham.ac.uk>
2018-03-05 08:49:06 +0000

    Got a basic now playing websocket working.

--- musicstats/consumers.py
+++ musicstats/consumers.py
@@ -4,11 +4,41 @@
 
 from channels import Group
 from channels.sessions import channel_session
-from musicstats.models import Station
+from musicstats.models import Station, SongPlay
+from musicstats.serializers import SongPlaySerializer
 from asgiref.inmemory import channel_layer
 import logging
+import json
 
 logger = logging.getLogger(__name__)
+
+# Sends the now playing
+
+def send_now_playing(station):
+    
+    # Sanity check
+    
+    if not station:
+        logger.debug('A station must be supplied in order to send the now playing info.')
+        return
+    
+    # Get the last song play
+    
+    play_query = SongPlay.objects.all()
+    play_query = play_query.filter(station__id=station.id)
+    play_query = play_query.order_by('-date_time')[:1]
+    
+    if len(play_query) == 0:
+        logger.debug('Could not find the last song played for {}.'.format(station))
+        return
+    
+    song_play = play_query[0]
+    song_play_serial = SongPlaySerializer(song_play)
+    
+    # Send the message to the channel
+    
+    logger.debug('Sending now playing for {}.'.format(station))
+    Group('station-{}'.format(station.id)).send({'text': json.dumps(song_play_serial.data)})
 
 @channel_session
 def ws_connect(message):
@@ -35,12 +65,16 @@ def ws_connect(message):
     # Add the user to the group
     
     logger.debug('Connecting client {}:{} to now playing for {}'.format(message['client'][0], message['client'][1], name))    
-    Group('station-{}'.format(station.id), channel_layer=message.channel_layer).add(message.reply_channel)
+    Group('station-{}'.format(station.id)).add(message.reply_channel)
     message.channel_session['station'] = name
+    
+    # Send the latest now playing to the channel
+    
+    send_now_playing(station)
     
 @channel_session
 def ws_receive(message):
-    logger.debug('Recieved message from user: {}'.format(message['text']))
+    logger.debug('Received message from user: {}'.format(message['text']))
     
 @channel_session
 def ws_disconnect(message):
@@ -49,7 +83,9 @@ def ws_disconnect(message):
         name = message.channel_session['station']
         station = Station.objects.get(name=name)
         Group('station-{}'.format(station.id), channel_layer=message.channel_layer).discard(message.reply_channel)
-        logger.debug('Disconnected client {}:{} to now playing for {}'.format(message['client'][0], message['client'][1], name))
         
     except (KeyError, Station.DoesNotExist):
-        logger.debug('Failed to get a valid station ({}) from the disconnecting user.'.format(name))
+        if (name):
+            logger.debug('Failed to get a valid station ({}) from the disconnecting user.'.format(name))
+        else:
+            logger.debug('Unable to disconnect client as no station was supplied.')
