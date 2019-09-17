dd1f31bdcd43278e41669ac055ddebb193619020
Marc Steele <marcsteele@Solid.local>
2019-09-17 21:41:39 +0100

    Security issue. Disable debugging by default.

--- musicstats/settings.py
+++ musicstats/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't4+uu#njhdjkjm@_h(w9dg5%3p@5pjm#n*45i+!in9o!1*x4!0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
